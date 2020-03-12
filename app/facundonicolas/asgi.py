77608aa6981eac64be18c9def43f20e266dc9878
facmartoni <thunderxforce@gmail.com>
2020-03-12 15:47:22 -0300

    asgi.py modified, added settings_prod

--- app/facundonicolas/asgi.py
+++ app/facundonicolas/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facundonicolas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facundonicolas.settings_prod')
 
 application = get_asgi_application()
