4f4c90a92060e186e404fcf2b98527297e60f8bc
Facundo (facundonicolas.com droplet) <thunderxforce@gmail.com>
2020-03-12 05:05:05 +0000

    Production Adjustments 1

--- app/manage.py
+++ app/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facundonicolas.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facundonicolas.settings_prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
